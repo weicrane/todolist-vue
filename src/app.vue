6949c073c0f630829c63b039bbd92dc18cb3022c
weicane <wyhweiyihe@163.com>
2020-03-03 11:30:28 +0800

    添加基本的webpack配置

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="test">{{testmessage}}</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            test:'abd'
+        }
+    }
+};
+</script>
+
+<style scoped>
+#test{
+    color:red;
+
+}
+</style>
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from 'vue'
+import './assets/style/test.css'
+import './assets/style/test-stylus.styl'
+import './assets/images/california-road-highway.jpg'
+
+const root = document.createElement('div')
+document.body.appendChild(root)
+new Vue({
+    render: (h) => h(App)
+}).$mount(root)
